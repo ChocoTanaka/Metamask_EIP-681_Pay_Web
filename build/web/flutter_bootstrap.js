6fe0fdb70b8bbff8f43e5310a39500a351d8f468
ChocoTanaka <straighter15@nifty.com>
2026-06-18 02:02:01 +0900

    change graphics_9

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3130042559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1701822314" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

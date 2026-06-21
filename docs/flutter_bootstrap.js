f15f0cffd581e4cb5a82177b6a0303bcc1f51dc1
ChocoTanaka <straighter15@nifty.com>
2026-06-22 03:26:45 +0900

    reown test12

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2334667994" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2338801911" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
